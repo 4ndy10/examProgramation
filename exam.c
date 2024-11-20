4eac8893badd5969b899dcf5160c6fc3af2be8a2
4ndy10 <andysoto101205@gmail.com>
2024-11-20 14:36:07 +1100

    First checkpoint

---
+++ exam.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main()
+{
+    //Validar el rango:
+        //Verifica que el valor inicial sea menor o igal al valor final.
+        //Si el rango no es válido, solicita al usuario que ingrese los valores
+        //nuevamente hasta que el rango sea correcta.
+
+    //Elegir una opción:
+        //Permite al usuario elegir entre las siguientes opciones:
+            //Mostrar todos los números pares en el rango y calcular la suma
+            //Mostrar todos los números impares en el rango y calcular su suma
+            //Mostrar tanto los números pares como los impares, junto con sus respectivas sumas
+
+    //Identificar y mostrar números
+        //Según la opción elegida, imprime los números correspondientes (pares, impares o ambos) y sus respectivas sumas.
+
+    //Estructura del programa:
+        //Utiliza funciones para modularizar las tareas:
+            //Una para verificar si un número es par.
+            //Otra para verificar si un número es impar.
+            //Otra para imprimir los números de un tipo específico (pares o impares) y calcular su suma
+            //Otra para manejar las opciones y delegar las tareas.
+
+
+
+
+}
